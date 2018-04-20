c19595a3f794e6ac3774b3d784ac9a84ca8413db
Terry Allitt <tallitt88@gmail.com>
2018-04-20 19:39:11 +0100

    :recycle: Wrap tests into semantic describe blocks

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -5,19 +5,43 @@ describe("FizzBuzz", function() {
     fizzBuzz = new FizzBuzz();
   });
 
-  it("should return a number that isn't divisible by 3 or 5", function() {
-    expect(fizzBuzz.play(13)).toEqual(13);
+  describe("multiples of 3", function() {
+    it("should return 'Fizz' when number is 3", function() {
+      expect(fizzBuzz.play(3)).toEqual('Fizz');
+    });
+
+    it("should return 'Fizz' when number is 6", function() {
+      expect(fizzBuzz.play(6)).toEqual('Fizz');
+    });
   });
 
-  it("should return 'fizz' for multiples of 3", function() {
-    expect(fizzBuzz.play(3)).toEqual('Fizz');
+  describe("multiples of 5", function() {
+    it("should return 'Buzz' when number is 5", function() {
+      expect(fizzBuzz.play(5)).toEqual('Buzz');
+    });
+
+    it("should return 'Buzz' when number is 5", function() {
+      expect(fizzBuzz.play(10)).toEqual('Buzz');
+    });
   });
 
-  it("should return 'buzz' for multiples of 5", function() {
-    expect(fizzBuzz.play(5)).toEqual('Buzz');
+  describe("multiples of 3 and 5", function() {
+    it("should return 'FizzBuzz' when number is 15", function() {
+      expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
+    });
+
+    it("should return 'FizzBuzz' when number is 30", function() {
+      expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
+    });
   });
 
-  it("should return 'fizzbuzz' for multiples of 3 and 5", function() {
-    expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
+  describe("numbers not divisible by 3 or 5", function() {
+    it("should return 8 when number is 8", function() {
+      expect(fizzBuzz.play(8)).toEqual(8);
+    });
+
+    it("should return 13 when number is 13", function() {
+      expect(fizzBuzz.play(13)).toEqual(13);
+    });
   });
 });
